b4f3b6fd00249fae35ca0d413c7408c5da8cd784
Vincent Wen <wensi@live.com>
2018-06-24 23:37:16 -0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-while true; do
-    cd /home/nvidia/Projects/tensorflow-face-detection
-    echo "Restarted at $(date +%Y%m%d_%H%M%S)" >> log
-    python inference_leapbox.py
-done
+cd /home/nvidia/Projects/tensorflow-face-detection
+python inference_leapbox.py
